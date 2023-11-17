f6830eaea4a7358b38e9092126b138a36ffaa544
世界 <aolijoin@gmail.com>
2023-11-17 10:52:23 +0800

    2023年11月16日审批

---
+++ src/views/main.vue
@@ -0,0 +1,25 @@
+<script>
+import order from "@/components/order.vue";
+import about from "@/components/about.vue";
+
+export default {
+  comments: {
+    order,
+    about
+  }
+}
+</script>
+
+<template>
+  <div id="main">
+    <nav>
+      <router-link to="/order">单据管理</router-link>
+      |
+      <router-link to="/about">审批单管理</router-link>
+    </nav>
+  </div>
+</template>
+
+<style>
+
+</style>
